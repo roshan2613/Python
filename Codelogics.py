a2794949d5db926c1d307064dfa44de434fb9a9d
Roshan <roshan2613@gmail.com>
2019-03-28 05:21:10 -0500

    wrote two programs: 1.Fibonacci series 2.Fizz buzz

--- Codelogics.py
+++ Codelogics.py
@@ -1,1 +1,49 @@
-#all the Python code to remember 
+#all the Python code to remember
+
+
+
+'''
+
+
+
+
+#1.fibonacci series: 0,1,1,2,3,5,8,13,21,34
+def fibb(n):
+    a = 0
+    b = 1
+
+    if n == 1:
+        print (b)
+    else :
+        print(a)
+        print (b)
+        for i in range(2,n):
+            c = a + b
+            a = b
+            b = c
+
+            print (c)
+fibb(12)
+
+'''
+
+
+
+
+'''
+
+#2.Fizz Buzz: print ("fizz") for all numbers divisible by 3 , print ("buzz") for all numbers divisible by 5
+# and finally print ("Fizz buzz") for numbers divisible by 3 and 5.
+def fizzbuzz (n):
+    for i in range (n):
+        if i % 15 == 0:
+            print ("Fizz Buzz")
+        elif i % 5 == 0:
+            print ("Buzz")
+        elif i % 3 == 0:
+            print ("Fizz")
+        else :
+            print (i)
+
+fizzbuzz (100)
+'''
